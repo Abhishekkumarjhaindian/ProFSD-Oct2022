3f3d03d834a0fb827cf7e98e2481b0d8b061ccfe
Abhishekkumarjhaindian <109462037+Abhishekkumarjhaindian@users.noreply.github.com>
2022-12-11 00:32:17 +0530

    Update TowerOfHanoi.java

--- src/InClassAssignments/Recursion/TowerOfHanoi.java
+++ src/InClassAssignments/Recursion/TowerOfHanoi.java
@@ -19,3 +19,28 @@ public class TowerOfHanoi {
     }
 
 }
+
+
+/*import java.util.*;
+
+class Main
+{
+    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+    {
+        if (n == 1)
+        {
+            System.out.println(n+":"+ from_rod + "->" + to_rod);
+            return;
+        }
+        towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+        System.out.println(n+":"+ from_rod + "->" + to_rod);
+        towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }
+}*/
